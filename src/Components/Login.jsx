a1140993ca52dba13ef4ca7c71fdf342a23f4461
Nikhilrenjith <42229967+Nikhilrenjith@users.noreply.github.com>
2023-12-08 19:58:38 +0530

    .

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -39,7 +39,7 @@ const Login = ({ handleChange }) => {
   });
   const paperStyle = {
     padding: 20,
-
+    height: "80vh",
     width: 500,
     margin: "0 auto",
   };
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -81,6 +81,7 @@ export default function SignUp() {
     padding: 20,
     width: 500,
     margin: "0 auto",
+    height: "90vh",
   };
   return (
     <Paper style={paperStyle}>
