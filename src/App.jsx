4a3a00f841fb5aa2a1c2eb1c5927540037e05ab6
Nikhilrenjith <42229967+Nikhilrenjith@users.noreply.github.com>
2023-12-08 21:05:24 +0530

    concluded

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Container from "./Components/Container";
-import LandingPage from "./Components/LandingPage";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact component={Container} />
-        <Route path="/landing" component={LandingPage} />
-      </Routes>
-    </Router>
+    <div
+      style={{
+        backgroundImage: `url(
+          "https://images.unsplash.com/photo-1540800458874-73e6a5eed8ac?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        )`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        padding: "20px",
+        height: "100vh",
+      }}
+    >
+      <Container />
+    </div>
   );
 };
 
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -16,12 +16,11 @@ import { FormHelperText } from "@mui/material";
 import * as Yup from "yup";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useNavigate } from "react-router-dom";
 
 const Login = ({ handleChange }) => {
   const errorTextStyle = { color: "red", marginLeft: -1 };
   const [showPassword, setShowPassword] = React.useState(false);
-  const navigate = useNavigate();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -61,8 +60,8 @@ const Login = ({ handleChange }) => {
 
       if (data.success) {
         localStorage.setItem("token", data.token);
+        console.log("Login successful");
         toast.success("Login successful");
-        navigate("/landing");
       } else {
         console.error("Login failed:", data.message);
         toast.error("Invalid Credentials");
